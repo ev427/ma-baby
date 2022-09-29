19cb44ebbe988392557c3f85912977e20b16fc4b
ev427 <111007327+ev427@users.noreply.github.com>
2022-09-29 11:01:26 -0700

    boonkles

--- Ma bay/Assets/scripts/jump.cs
+++ Ma bay/Assets/scripts/jump.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class jump : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 10;
+    public float jumpSpeed = 20;
+
+    public bool isOnGround;
+
+    private Rigidbody2D _myRB;
+    private Collider2D _myCollider;
+
     void Start()
     {
-        
+        _myRB = GetComponent<Rigidbody2D>();
+        _myCollider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        _myRB.velocity = new Vector2(horizontalInput * moveSpeed, _myRB.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            _myRB.velocity = new Vector2(_myRB.velocity.x, jumpSpeed);
+        }
     }
 }
